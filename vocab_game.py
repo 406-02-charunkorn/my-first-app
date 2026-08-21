08cc149b3c40c9f92c81588ff2ae0e7a98fd85fd
406-02-charunkorn <54157@yupparaj.ac.th>
2026-08-21 11:05:27 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -97,13 +97,6 @@ ans2 = st.text_input(
     "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
     value=st.session_state.ans2_val,
 )
-
-# อัปเดตค่าล่าสุดเข้าตัวแปร
-st.session_state.ans1_val = ans1
-st.session_state.ans2_val = ans2
-st.session_state.ans3_val = ans3
-st.session_state.ans4_val = ans4
-
 ans3 = st.text_input(
     "ข้อ 3: Pinky love to `t _ n _ p a _ m`. 🌴",
     value=st.session_state.ans3_val,
@@ -112,6 +105,11 @@ ans4 = st.text_input(
     "ข้อ 4: Tonpalm love to `p _ _ k y`. 🩷",
     value=st.session_state.ans4_val,
 )
+# อัปเดตค่าล่าสุดเข้าตัวแปร
+st.session_state.ans1_val = ans1
+st.session_state.ans2_val = ans2
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
 
 # 4. ปุ่มส่งคำตอบ
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
