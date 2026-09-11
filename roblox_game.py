c74ebb38b3a262fbf07c2e46d67efce8ef85bc69
406-02-charunkorn <54157@yupparaj.ac.th>
2026-09-11 11:02:45 +0700

    Update roblox_game.py

--- roblox_game.py
+++ roblox_game.py
@@ -31,6 +31,7 @@ def reset_game():
     st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 2
     st.session_state.start = time.time()  # เริ่มเวลาใหม่
     st.session_state.is_ended = False  # ปิด Dialog
+    st.session_state.jumpscare = "" 
 
 
 # ----------------------------------------------------
